e08f31a0ce676626d62d0de932f1914221cf2a6d
dillonscroggin@gmail.com <dillonscroggin@gmail.com>
2013-03-15 16:03:59 -0400

    Player class complete and tested

--- Chandra-working/Player.cc
+++ Chandra-working/Player.cc
@@ -4,15 +4,18 @@ class Player{
     private:
 	Deck* deck;
 	Library* libr;
+	bool lost;
+	int life;
     public:
 	Hand* hand;
 	void* graveyard;
 	void draw();
-	int life;
+	void damage(int);
 	Player(Deck*);
 	~Player();
 	void print();
-}
+	bool hasLost();
+};
 
 Player::Player(Deck* d){
     deck = d;
@@ -20,11 +23,38 @@ Player::Player(Deck* d){
     hand = new Hand();
     graveyard = 0;
     life = 20;
-}
+    lost = false;
+};
 
 Player::~Player(){
     delete libr;
     delete hand;
+};
+
+void Player::draw(){
+    Card* drawn = libr->draw();
+    if(drawn == 0){
+	lost = true;
+	return;
+    }
+    hand->add(drawn);
+};
+
+void Player::damage(int n){
+    life = life - n;
+    if(life <= 0)
+	lost = true;
+};
+
+void Player::print(){
+    cout<<"Printing player-\n";
+    cout<<"  Lost: "<<lost<<'\n';
+    cout<<"  Life: "<<life<<'\n';
+    deck->print();
+    hand->print();
+    cout<<"End player\n";
+};
+
+bool Player::hasLost(){
+    return lost;
 }
-
-
